230261fa336092f070b59ce4e4cc127924abe7a1
Clonebx <clonebx.1@gmail.com>
2019-08-24 14:23:45 +0700

    add navbar

--- app/src/main/java/com/example/formoms/MainActivity.java
+++ app/src/main/java/com/example/formoms/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.formoms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting_profile);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.navigation);
+        navView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+//            case R.id.navigation_care:
+//                fragment = new HomeFragment();
+//                break;
+            case R.id.navigation_memories:
+                fragment = new MemoriesFragment();
+                break;
+//            case R.id.navigation_notif:
+//                fragment = new NotificationFragment();
+//                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
